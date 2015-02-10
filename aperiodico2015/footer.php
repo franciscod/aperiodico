87c2116d8dd36e93550b357988af479abeb2e59b
Francisco Demartino <demartino.francisco@gmail.com>
2015-02-10 06:17:17 -0300

    remove some stuff

--- aperiodico2015/footer.php
+++ aperiodico2015/footer.php
@@ -12,9 +12,7 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'aperiodico2015' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'aperiodico2015' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'aperiodico2015' ), 'aperiodico2015', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
+			
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
