c271a3bf3bd708a878618afef24709308cf85973
Francisco Demartino <demartino.francisco@gmail.com>
2015-02-26 12:48:39 -0300

    fix included template file
    was sending content before headers

--- inc/aperiodico-templates.php
+++ inc/aperiodico-templates.php
@@ -1,14 +1,18 @@
-<?php function apsi_social() { ?>
+<?php
+
+function apsi_social() { ?>
 	<div class="social">
 		<a target="_blank" class="fb" href="https://www.facebook.com/Aperiodico.inconscient">Facebook/Aperiódico.Psicoanalítico</a>
 		<a target="_blank" class="tw">Twitter/Aperiódico.Psicoanalítico</a>
 	</div>
-<?php } ?>
+<?php }
 
-<?php function apsi_pie_post() { ?>
+function apsi_pie_post() { ?>
 	<footer class="entry-footer">
 		<div class="bottom_aligner"></div>
 		<div class="line">Donde conseguir el numero?</div>
 		<div class="line">Compartir FB TW</div>
 	</footer>
-<?php } ?>
+<?php } 
+
+?>
