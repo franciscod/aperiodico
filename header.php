a1089280343a5014b68729e7b011c4b7feecb66b
Francisco Demartino <demartino.francisco@gmail.com>
2015-04-06 06:14:41 -0300

    add google analytics

--- header.php
+++ header.php
@@ -19,6 +19,14 @@
 if(navigator.userAgent.match(/iPhone/i)) {
 	$('meta[name=viewport]').attr('content', 'width=device-width, initial-scale=0.47, minimum-scale=0.47');
 }
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-61497096-1', 'auto');
+ga('send', 'pageview');
 </script>
 </head>
 
